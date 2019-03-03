859464ac008f5250921e41160eee98af8b5f93ba
Excalib <rave9373@gmail.com>
2019-03-03 12:01:15 +0500

    Init solution, added projects

---
+++ src/PublisherBot.Scheduler/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PublisherBot.Scheduler
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/PublisherBot.TelegramAPI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PublisherBot.TelegramAPI
+{
+    public class Class1
+    {
+    }
+}
